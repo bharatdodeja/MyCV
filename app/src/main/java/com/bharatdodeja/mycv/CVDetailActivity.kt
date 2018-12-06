053ab29824d3a9190a52e5e034b5abcdfbc68bf7
Dodeja, Bharat <ven-bdodeja@express.com>
2018-12-06 13:06:12 +0530

    Initial commit

---
+++ app/src/main/java/com/bharatdodeja/mycv/CVDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.bharatdodeja.mycv
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CVDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cv_detail)
+    }
+}
